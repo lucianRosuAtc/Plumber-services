a9126faa68e1d921ea8a17b89d6c752ab06bde9a
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-04 16:43:30 +0100

    refactor: Update npm dependencies, add '@radix-ui/react-dialog' and '@radix-ui/react-slot' packages, and improve ImageSlider component

---
+++ components/NavBar.tsx
@@ -0,0 +1,24 @@
+'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { navigationlinks } from "./components-data/links-data";
+
+export default function NavBar() {
+  const pathname = usePathname();
+  return (
+    <div className="hidden xl:flex gap-x-8 items-center">
+      {navigationlinks.map((navlink) => (
+      <Link className={`link hover:text-primary pb-1 ${pathname === navlink.url ? 'active' : ''}`}
+          key={navlink.url}
+          href={navlink.url}
+        >
+          {navlink.name}
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+
+
