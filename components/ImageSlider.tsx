5ebf8063e23b4b9e4e50dd663ac477651463d0b0
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-04 09:48:10 +0100

    refactor: Update CSS and component names for consistency

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -1,8 +1,8 @@
-'use client'
 
+"use client";
 
-import React, { useState } from 'react';
-import { imageSliderData } from './imageSlider-data';
+import React, { useState } from "react";
+import { imageSliderData } from "./imageSlider-data";
 
 export default function Images() {
   const [selectedCard, setSelectedCard] = useState(imageSliderData[0].id);
@@ -13,27 +13,39 @@ export default function Images() {
 
   return (
     <div className="w-full h-screen flex items-center justify-center">
-     <div className="h-96 flex flex-nowrap justify-start">
-      
+      <div className="h-96 flex flex-nowrap justify-start">
         {imageSliderData.map((data, index) => (
           <React.Fragment key={data.id}>
-            <input type="radio" name="slide" id={data.id} checked={selectedCard === data.id} className="hidden" />
+            <input
+              type="radio"
+              name="slide"
+              id={data.id}
+              checked={selectedCard === data.id}
+              className="hidden"
+            />
 
-            <label htmlFor={data.id} className={`card ${selectedCard === data.id ? 'w-96' : 'w-20'} bg-cover cursor-pointer overflow-hidden flex items-end transition-all duration-600 ease-in-out shadow-lg m-2`} 
-            
-            onClick={() => handleCardClick(data.id)} style={{backgroundImage: `url('${data.image}')`}}>
+            <label
+              htmlFor={data.id}
+              className={`rounded-2xl ${
+                selectedCard === data.id ? "w-[600px]" : "w-[70px]"
+              } bg-cover cursor-pointer overflow-hidden flex items-end transition-all duration-600 ease-in-out shadow-lg m-2`}
+              onClick={() => handleCardClick(data.id)}
+              style={{ backgroundImage: `url('${data.image}')` }}
+            >
+              <div className="text-white flex flex-nowrap">
+                <div className="bg-gray-800 text-white rounded-full w-12 h-12 flex items-center justify-center m-3">
+                  {index + 1}
+                </div>
 
-
-
-              <div className="row text-white flex">
-
-                <div className="icon bg-gray-800 rounded-full w-12 h-12 flex items-center justify-center m-3">{index + 1}</div>
-
-                <div className={`description flex items-center justify-center flex-col overflow-hidden h-20 w-64 ${selectedCard === data.id ? 'opacity-100 transform-none delay-300' : 'opacity-0 -translate-y-8'} transition-all duration-300 ease-in-out`}>
-
+                <div
+                  className={`flex items-center justify-center flex-col overflow-hidden h-20 w-[520px] ${
+                    selectedCard === data.id
+                      ? "opacity-100 transform-none delay-300"
+                      : "opacity-0 -translate-y-[30px]"
+                  } transition-all duration-300 ease-in-out delay-300`}
+                >
                   <h4 className="uppercase font-bold mr-auto">{data.title}</h4>
                   <p className="text-gray-200 pt-1">{data.description}</p>
-
                 </div>
               </div>
             </label>
@@ -43,48 +55,3 @@ export default function Images() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
