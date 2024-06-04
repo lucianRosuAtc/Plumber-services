17076abb6e60a79bb08322fa4e8eb99284ce7880
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-04 16:53:12 +0100

    refactor: Update import path for ImageSlider component and add ScrollToTopButton to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { ThemeProvider } from "@/components/ThemeProvider";
+import ScrollToTopButton from "../components/ScrollToTopButton";
+import { ThemeProvider } from "../components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,12 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={`${inter.className} max-w-[1920px] mx-auto`}>
-         <ThemeProvider attribute='class'  defaultTheme="system">
-           <Header />
-        {children}
-      <Footer />
-      </ThemeProvider> 
+      <body className={`${inter.className} max-w-[1920px] mx-auto`}>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <Header />
+          {children}
+          <ScrollToTopButton />
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
