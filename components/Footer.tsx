8ecf0e27d0850b8343509b28885217cc6e81593a
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-04 14:44:23 +0100

    refactor: Add Footer component to layout and update page component

---
+++ components/Footer.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from "next/link";
+import Logo from "./components-data/Logo";
+import { social, navigationlinks } from "./components-data/links-data";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const date = new Date();
+const year = date.getFullYear();
+
+export default function Footer() {
+  const pathname = usePathname();
+  return (
+    <div className="bg-secondary/90 dark:bg-secondary pt-8 border-t border-primary shadow-xl">
+      <div className="grid lg:grid-cols-3 justify-center items-center gap-y-8">
+        <div className="mx-auto">
+          <Logo />
+        </div>
+
+        {/* MARK: Navigation Links
+         */}
+        <div className="flex gap-x-8 items-center justify-center">
+          {navigationlinks.map((navlink) => (
+            <Link
+              className={`link text-white hover:text-primary pb-1 ${
+                pathname === navlink.url ? "active" : ""
+              }`}
+              key={navlink.url}
+              href={navlink.url}
+            >
+              {navlink.name}
+            </Link>
+          ))}
+        </div>
+
+        {/* MARK: Social Media
+         */}
+        <div className="flex gap-x-8 items-center justify-center">
+          {social.map((item, index) => (
+            <a
+              href={item.url}
+              key={index}
+              target="_blank"
+              className="text-primary hover:text-orange-100 dark:text-orange-100 dark:hover:text-primary text-3xl transition-all"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center px-4 text-white py-8 lg:pt-14">
+        <p className="text-sm text-center">
+          <span className="pr-1">&copy;</span>{" "}
+          {`${year} Lucian Dev, All rights reserved.`}{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ components/components-data/testimonial-data.tsx
@@ -0,0 +1,39 @@
+export const testimonialData = [
+    {
+      img: "/img/recommendations/Amber.jpg",
+      name: "Amber Robeck",
+      job: "Full Stack Web Developer @ edX",
+      description:
+        "Lucian is a fast learner with a unique approach to solving problems. Lucian is not afraid to dive into challenging tasks with a positive attitude. He is a great contributor to any team. I look forward to working with Lucian in the future and highly recommend him for any projects.",
+    },
+    {
+      img: "/img/recommendations/Jade.jpeg",
+      name: "Jade Page",
+      job: "Senior Web Developer @ Adopstar Ltd",
+      description:
+        "An absolute pleasure to work with, Lucian Rosu radiates positivity and kindness, fostering a harmonious and supportive work environment effortlessly.",
+    },
+    {
+      img: "/img/recommendations/Gilson.jpg",
+      name: "Gilson de Almeida",
+      job: "Full Stack Developer",
+      description:
+      "Lucian collaborated with us on a project. During this period, he proved that he is an ambitious, hardworking person with good technical skills who has a strong desire to learn even more and improve himself. He works very well in the team and is always willing to help. Lucian is a person I highly recommend.",
+    },
+    {
+      img: "/img/recommendations/AlexErnila.jpg",
+      name: "Alex Ernila",
+      job: "Software Developer @ Continental",
+      description:
+      "Lucian Rosu is one of the best among all the people I have ever worked with. He is a very productive person, hardworking, broad-minded and forward-thinking individual. Intelligent, ambitious, energetic and proactive perfectionist. The desire for proficiency make Lucian a valuable asset to the team.",
+    },
+  
+    {
+      img: "/img/recommendations/Kieran.jpeg",
+      name: "Kieran Colley",
+      job: "Senior Full Stack React/NextJs Developer @ Adopstar Ltd",
+      description:
+        "Lucian is a great person to work with, always wanting to learn new things and get stuck in. He has a great personality and a great work ethic.",
+    },
+  ];
+  
