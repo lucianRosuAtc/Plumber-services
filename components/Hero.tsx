d9d51f609b790d4589205ad4e48c858b86720029
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-05 15:53:55 +0100

    refactor: Update Hero component with lazy loading for hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function Hero() {
   return (
-    <div>
-    <div className=''>
-      <div className="">PARALLAX</div>
-    </div>
- 
-    </div>
-  )
+
+      <div className="">
+        <Image
+        src="/images/parallax/theplumber.webp"
+        width="600"
+        height="500"
+        alt="Plumber fixing a pipe"
+        className="w-full h-[800px] object-cover"
+        />
+        <div className="hero-text">PARALLAX</div>
+      </div>
+
+  );
 }
