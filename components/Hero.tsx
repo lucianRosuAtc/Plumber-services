e3fe89a357bfc006769627a823218cf888f50e64
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-05 16:23:38 +0100

    refactor: Update Hero component with improved layout and lazy loading for hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,17 +3,23 @@ import React from "react";
 
 export default function Hero() {
   return (
-
-      <div className="">
-        <Image
+    <div className="relative">
+      <div className="absolute flex flex-col justify-end pb-10 md:pb-20 text-white mx-4 md:mx-8 uppercase h-full">
+        <h4 className="text-2xl lg:text-7xl leading-loose">
+          Plumming & electric services
+        </h4>
+        <p className="text-lg md:text-2xl leading-loose">
+          We offer professional services search firm built on integrity,
+          experience and a will to deliver exceptional talent.
+        </p>
+      </div>
+      <Image
         src="/images/parallax/theplumber.webp"
         width="600"
         height="500"
         alt="Plumber fixing a pipe"
-        className="w-full h-[800px] object-cover"
-        />
-        <div className="hero-text">PARALLAX</div>
-      </div>
-
+        className="w-full h-[500px] md:h-[800px] object-cover"
+      />
+    </div>
   );
 }
