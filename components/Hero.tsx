73fbb3eee7869f36885cc14aa117232c710b86c2
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-05 15:31:30 +0100

    refactor: Remove unused parallax CSS code and update Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,15 +2,11 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <>
-    <div className='section1'>
-      <div className="text">PARALLAX</div>
+    <div>
+    <div className=''>
+      <div className="">PARALLAX</div>
     </div>
-    <div className='section2'>
-      <div className="text">Normal</div>
+ 
     </div>
-    </>
   )
 }
-
-
