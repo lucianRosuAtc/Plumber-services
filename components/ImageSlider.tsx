f06471651ca9512f624be2849a9e48eaa8cf1baf
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-05 18:19:24 +0100

    refactor: Update ImageSlider component with responsive height and background color

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -12,7 +12,7 @@ export default function Images() {
   };
 
   return (
-    <div className="w-full h-screen flex  items-center justify-center">
+    <div className="w-full h-[550px] flex items-center justify-center bg-orange-200 my-6 md:my-12">
       <div className="h-[500px] md:h-96 flex md:flex-row flex-col flex-nowrap justify-start">
         {imageSliderData.map((data, index) => (
           <React.Fragment key={data.id}>
